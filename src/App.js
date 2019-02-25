d65fd5287b5d8816ff9566a056560d3026c54b84
Ben Mirkhah <bmirkhah@belus2204-mb15.local>
2019-02-25 03:41:58 -0800

    Switch to SCSS instead of css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 import RealTimeDepartures from './components/realtimedepartures';
 import {TripPlanner} from './components/tripplanner/TripPlanner';
