e09f03f1c8747aafa351c148ffc79dddc6780399
Jonathan Gee <j@jonathangee.com>
2021-02-13 16:06:28 -0800

    feat: stubbed out the trip planning

--- src/components/tripPlanner/leg.js
+++ src/components/tripPlanner/leg.js
@@ -1,16 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, Typography } from '@material-ui/core';
+import { getFormattedTime } from '../../utils/date';
 
-const Leg = ({origin,destination,date,}) => {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    flex: 1,
+  },
+});
 
-Leg.propTypes = {
+const Leg = ({ origin, destination, destDate, origDate, bikeflag, line }) => {
+  const classes = useStyles();
 
-}
+  return (
+    <div className={classes.root}>
+      <div>
+        <Typography variant="body2">{getFormattedTime(origDate)}</Typography>
+        <Typography variant="body2">{getFormattedTime(destDate)}</Typography>
+      </div>
+      <Typography variant="body2">
+        {line} bike:{bikeflag}
+      </Typography>
+      <div>
+        <Typography variant="body2">{origin?.name}</Typography>
+        <Typography variant="body2">{destination?.name}</Typography>
+      </div>
+    </div>
+  );
+};
 
-export default Leg
+Leg.propTypes = {};
+
+export default Leg;
--- src/components/tripPlanner/trip.js
+++ src/components/tripPlanner/trip.js
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, Typography } from '@material-ui/core';
 import { getFormattedTime } from '../../utils/date';
+import AttachMoneyTwoToneIcon from '@material-ui/icons/AttachMoneyTwoTone';
+import AccessTimeTwoToneIcon from '@material-ui/icons/AccessTimeTwoTone';
 import Logger from 'js-logger';
+import Leg from './leg';
 
 const useStyles = makeStyles({
-  root: {
+  root: {},
+  header: {
     display: 'flex',
     justifyContent: 'space-between',
     flex: 1,
   },
+  details: {
+    display: 'flex',
+  },
+  leg: {},
 });
 
 const Trip = ({
@@ -26,15 +34,32 @@ const Trip = ({
 
   return (
     <div className={classes.root}>
-      <div>
-        <Typography variant="h5">{getFormattedTime(origDate)}</Typography>
-        <Typography variant="h5">{getFormattedTime(destDate)}</Typography>
+      <div className={classes.header}>
+        <div>
+          <Typography variant="h5">{getFormattedTime(origDate)}</Typography>
+          <Typography variant="h5">{getFormattedTime(destDate)}</Typography>
+        </div>
+        <div className={classes.details}>
+          <div className={classes.details}>
+            <AccessTimeTwoToneIcon fontSize="small" />
+            <Typography variant="body1">{tripTime}</Typography>
+          </div>
+          <div className={classes.details}>
+            <AttachMoneyTwoToneIcon fontSize="small" />
+            <Typography variant="body1">{fare}</Typography>
+          </div>
+        </div>
+        <div>
+          <Typography variant="body2">{origin?.name}</Typography>
+          <Typography variant="caption">{origin?.address}</Typography>
+          <Typography variant="body2">{destination?.name}</Typography>
+          <Typography variant="caption">{destination?.address}</Typography>
+        </div>
       </div>
-      <div>
-        <Typography variant="body2">{origin?.name}</Typography>
-        <Typography variant="caption">{origin?.address}</Typography>
-        <Typography variant="body2">{destination?.name}</Typography>
-        <Typography variant="caption">{destination?.address}</Typography>
+      <div className={classes.leg}>
+        {leg.map((leg, index) => (
+          <Leg key={`leg_${index}`} {...leg} />
+        ))}
       </div>
     </div>
   );
