9f36760944461dd3e5973a8d02fff37042d63f34
Jonathan <j@jonathangee.com>
2021-08-21 21:10:03 -0700

    feat: use prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-    return (
-        <div>
-         application   
-        </div>
-    )
-}
+  return <div>application</div>;
+};
 
 export default App;
