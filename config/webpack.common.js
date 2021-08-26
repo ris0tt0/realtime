956dcf34bd515780eaa628fbd7623c3f81df805c
Jonathan <j@jonathangee.com>
2021-08-25 19:01:25 -0700

    chore: updated config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     app: './src/index.js',
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, '../dist'),
     clean: true,
   },
